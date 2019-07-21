c76fdc121f2e3d77fa9314935109d76116f23c0e
Priyam <priyam.ds@somaiya.edu>
2019-07-21 16:58:11 +0530

    removing node_modules

--- node_modules/vertex-camo/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-exports.connect = require('./lib/db').connect;
-exports.getClient = require('./lib/clients').getClient;
-
-exports.Document = require('./lib/document');
-exports.EmbeddedDocument = require('./lib/embedded-document');
