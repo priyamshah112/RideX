a116be078deaf93154503b592c9360adbb8c8f70
priyamshah112 <priyamshah112@gmail.com>
2019-08-04 01:27:17 +0530

    final app

---
+++ app/src/main/java/com/example/ridex/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.ridex;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    WebView myWebView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myWebView=(WebView)findViewById(R.id.webview);
+        WebSettings websettings=myWebView.getSettings();
+        websettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("https://ridex.herokuapp.com/");
+        myWebView.setWebViewClient(new WebViewClient());
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (myWebView.canGoBack()) {
+            myWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
--- express_app/controllers/create_identity.js
+++
@@ -1,6 +0,0 @@
-const ethCrypto=require('eth-crypto');
-module.exports=()=>{
-    var identity=ethCrypto.createIdentity();
-
-    return identity;
-}
